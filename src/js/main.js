27b57b66ede130c34be334d0fe8688ef9442a376
sgpotts <gpotts@news-0700ng.seatimes.com>
2015-07-17 12:16:19 -0700

    buttons working

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,7 @@ var L = element.leaflet;
 var map = element.map;
 
 var data = require("./blocks2.geo.json");
+var parcels = require("./test.geo.json");
 var $ = require('jquery');
 
 //ICH code for popup template if needed----------
@@ -18,6 +19,9 @@ ich.addTemplate("popup", templateFile);
 var onEachFeature = function(feature, layer) {
   layer.bindPopup(ich.popup(feature.properties))
 };
+
+
+var geojson;
 
  map.scrollWheelZoom.disable();
 
@@ -31,6 +35,13 @@ var onEachFeature = function(feature, layer) {
  function hhColor(d) {
     return d > 160000   ? '#333333' :
            d > 110000   ? '#333333' :
+           // d > 70000   ? '#9152D3' :
+                      '#transparent';
+}
+
+ function nhwColor(d) {
+    return d > 0.9   ? '#333333' :
+           // d > 0.801   ? '#333333' :
            // d > 70000   ? '#9152D3' :
                       '#transparent';
 }
@@ -56,6 +67,67 @@ function hhstyle(feature) {
     };
 }
 
-var base = L.geoJson(data, {style: style, onEachFeature:onEachFeature}).addTo(map);
+function nhwstyle(feature) {
+    return {
+        fillColor: "transparent",
+        weight: 2,
+        opacity: 1,
+        color: nhwColor(feature.properties.Pct_White),
+        fillOpacity: 0.7
+    };
+}
 
-var layer1 = L.geoJson(data, {style: hhstyle, onEachFeature:onEachFeature}).addTo(map);
+var base = L.geoJson(data, {style: style, onEachFeature:onEachFeature}).addTo(map);
+var layerhh = L.geoJson(data, {style: hhstyle, onEachFeature:onEachFeature});
+var layernhw = L.geoJson(data, {style: nhwstyle, onEachFeature:onEachFeature});
+
+var parceldata = L.geoJson(parcels, {
+   style: function (feature) {
+        return {color: 'black', fillColor: "black", weight: 0.5, fillOpacity: 1};
+    },
+});
+
+$(".type-button").click(function() {
+
+  $("#menu a.selected").removeClass("selected");
+  $(this).addClass("selected");
+  var type = $(this).data("type");
+  document.body.className = type;
+ 
+
+  if (type =="view_lots") {
+    parceldata.addTo(map);
+    map.removeLayer(layerhh);
+    map.removeLayer(layernhw);
+  }
+
+   if (type =="med_hh") {
+    layerhh.addTo(map);
+    map.removeLayer(parceldata);
+    map.removeLayer(layernhw);
+  }
+
+  if (type =="nhw_pop") {
+    layernhw.addTo(map);
+    map.removeLayer(parceldata);
+    map.removeLayer(layerhh);
+  }
+
+});
+
+window.toggle = false;
+
+$( ".name-button" ).on( "click", function() {
+
+   if(!toggle) {
+    map.removeLayer(base);
+  } else {
+    map.addLayer(base);
+  }
+  toggle = !toggle;
+ 
+
+ // base.toggle(this.checked);
+ 
+});
+
